b41ca90a8f976efcb8a6cdff9e01c619dd11405d
davilanter <davibarbosa.studiogames@gmail.com>
2026-03-27 18:53:17 -0300

    Create ATIVIDADEREVISAO.py

---
+++ ATIVIDADEREVISAO.py
@@ -0,0 +1,11 @@
+jeferson = "eu sei fazer print assim"
+impos = "e assim:"
+print (jeferson + " Hello Word!")
+print (impos + " Hello Word!")
+pedro = "24"
+manuel_gomes = "7"
+print (pedro + " e a quntidade de caracteries de jeferson ")
+print (manuel_gomes + " e a quntidade de caracteries de impos ")
+caneta_azul = 24 % 7
+print ("  a musica do manuel ")
+print (caneta_azul)
