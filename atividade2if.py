b5b6455541c4fb5ac35e12773fc846e8ce9c4b02
davilanter <davibarbosa.studiogames@gmail.com>
2026-03-27 19:52:24 -0300

    Create atividade2if.py

---
+++ atividade2if.py
@@ -0,0 +1,8 @@
+flechas = 100
+vida_inimigo = 100
+if vida_inimigo > flechas :
+    print("tu morreu kkkjkjkjkkk")
+if vida_inimigo < flechas :
+    print("O MISERAVEL E UM MISERAVEL, OK...VOCÊ MATOU...")
+if vida_inimigo == flechas :
+    print(":3 TUDO QUE VAI VOLTA, TU MATO ELE MAS TU TA POBRE")
