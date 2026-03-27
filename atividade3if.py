2ebf76c89491d539e7097c090dccd4b46d84dc7a
davilanter <davibarbosa.studiogames@gmail.com>
2026-03-27 20:18:10 -0300

    Create atividade3if.py

---
+++ atividade3if.py
@@ -0,0 +1,9 @@
+chovendo = True
+nublado = True
+if chovendo == True :
+    print ("leva um casaco de chuva porque ta chovendo")
+else: 
+    if nublado == True :
+        print ("leva um casaco de chuva porque ta nublado")
+    else:
+        print("não leva um casaco de chuva ")
