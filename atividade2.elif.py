7c2de7c0d09ca8480b8f9f23ca03895ea2220257
davilanter <davibarbosa.studiogames@gmail.com>
2026-04-10 20:40:54 -0300

    Create atividade2.elif.py

---
+++ atividade2.elif.py
@@ -0,0 +1,13 @@
+nota = int(input("Digite sua nota: "))
+
+
+if nota < 6 :
+    print("F")
+elif nota < 7:
+    print("D")
+elif nota < 8:
+    print("C")
+elif nota < 9:
+    print("B")
+else:
+    print("A")
